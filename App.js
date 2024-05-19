6845cde2362d9c8d7c1f911d1523f9383f53ee7a
DogukanAlperen <dogukanalperen441@gmail.com>
2024-05-19 18:01:44 +0300

    Redux ile state yapılarımıza ulaşmak ve erişilmesini kolaylaştırılmıştır

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import AnaNavigator from './android/app/src/navigation/anaNavigator';
-
+import { Provider } from 'react-redux';
+import { store } from './android/app/src/redux/store';
 
 
 const App = () => {
-  return <AnaNavigator/>
+ 
+  return 
+  
+  <Provider store={store}>
+    
+    <AnaNavigator/>
+  
+  </Provider>
 }
 
 export default App
